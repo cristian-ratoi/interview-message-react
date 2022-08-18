6a69cf1d3991b20ebd4f4447af214594f4f90cd5
cristian-ratoi <cristian.ratoi@vitals.co>
2022-08-18 16:35:15 +0300

    2 files modified, 3 added, 4 deleted

--- index.tsx
+++ index.tsx
@@ -1,9 +1,8 @@
-import * as React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import App from './app';
+import App from './src/app';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
--- message.api.ts
+++
@@ -1,9 +0,0 @@
-function wait(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-export async function getMessage(id: number): Promise<{ message: string }> {
-  await wait(id * 1000);
-
-  return { message: `Message for ${id}` };
-}
